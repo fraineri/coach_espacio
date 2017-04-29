05880b6a5cdb1cf4090e607f5bf166f530e009f7
Lucia <luuciarra@gmail.com>
2017-04-29 13:44:11 -0300

    home

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
 		<link rel="stylesheet" type="text/css" href="css/main.css">
 		<link rel="stylesheet" href="css/font-awesome/css/font-awesome.min.css">
 
-		<link href="https://fonts.googleapis.com/css?family=Yantramanav|Righteous" rel="stylesheet"> 
+		<link href="https://fonts.googleapis.com/css?family=Yantramanav" rel="stylesheet"> 
 
 		<meta name="viewport" content="width=device-width, initial-scale=1">
 
@@ -37,7 +37,7 @@
 		<div class ="section-container">
 			<!-- ABOUT -->
 			<?php 
-				$aboutIntro = [
+				/*$aboutIntro = [
 					'<p>Somos una organización especializada en coaching de espacios. Nosotros te podremos guiar y enseñar cómo acomodar el lugar deseado. Te ayudaremos a descubrir las cosas que ya no utilizas para que te deshagas de ellas y poder aprovechar y disfrutar al máximo tu espacio.</p>',
 					'<h2 class="intro-description-title">¿No tenés tiempo para acomodar la habitación?</h2>
 							<p>Nosotros pensamos por vos! Te ayudaremos a notar qué cosas ya no son necesarias en tu lugar. Podrás aprender a ver en tu espacio distintas soluciones para disfrutarlo lo mejor posible.</p>',
@@ -49,13 +49,59 @@
 							<p>Es importante tener una buena presentación. Te ayudaremos a hacer de tu lugar un espacio ameno para vos y tus clientes!</p>',
 					'<h2 class ="intro-description-title">Donamos lo que no necesitás</h2>
 							<p>Una vez terminados nuestros encuentros podrás optar por donar objetos, ropas, etc a hogares para que ellos lo disfruten. Nosotros nos encargamos de entregarlo!.</p>'
-				];
+				];*/
 			?>
 			<div id ="knowus" class ="mainSection about">
 				<h1 class ="page-title">Quiénes somos</h1>
 				<div class="about-intro">
-				<?php  
-					for ($i=0; $i < count($aboutIntro); $i++) { 
+					<div class ="align-info">
+						<div>
+							<p>Somos una organización especializada en coaching de espacios. Nosotros te podremos guiar y enseñar cómo acomodar el lugar deseado. Te ayudaremos a descubrir las cosas que ya no utilizas para que te deshagas de ellas y poder aprovechar y disfrutar al máximo tu espacio.</p>
+						</div>
+					</div>
+
+					<div class ="about-intro-description">
+						<div>
+							<img src="images/icons/reloj(1).png">
+							<h2 class="intro-description-title">¿No tenés tiempo para acomodar la habitación?</h2>
+							<p>Nosotros pensamos por vos! Te ayudaremos a notar qué cosas ya no son necesarias en tu lugar. Podrás aprender a ver en tu espacio distintas soluciones para disfrutarlo lo mejor posible.</p>
+						</div>
+					</div>
+
+					<div class ="about-intro-description">
+						<div>
+							<img src="images/icons/dormir.png">
+							<h2 class="intro-description-title">¿Llegás todos los días a tu casa tarde?</h2>
+							<p>Podremos hacer las reuniones durante el fin de semana para que no pierdas tiempo.</p>
+						</div>
+					</div>
+
+					<div class ="about-intro-description">
+						<div>
+							<img src="images/icons/percha.png">
+							<h2 class ="intro-description-title">¿No sabes por dónde arrancar en tu placard?</h2>
+							<p>Nosotros te ayudamos a limpiarlo y a implementar distintos métodos para que facilmente mantengas el orden.</p>
+						</div>
+					</div>
+
+					<div class ="about-intro-description">
+						<div>
+							<img src="images/icons/sillon.png">
+							<h2 class ="intro-description-title">¿Tenés una oficina nueva?</h2>
+							<p>Es importante tener una buena presentación. Te ayudaremos a hacer de tu lugar un espacio ameno para vos y tus clientes!</p>
+						</div>
+					</div>
+
+					<div class ="about-intro-description">
+						<div>
+							<img src="images/icons/camion-de-reparto.png">
+							<h2 class ="intro-description-title">Donamos lo que no necesitás</h2>
+							<p>Una vez terminados nuestros encuentros podrás optar por donar objetos, ropas, etc a hogares para que ellos lo disfruten. Nosotros nos encargamos de entregarlo!.</p>
+						</div>
+					</div>
+					
+				<!--<?php
+					/*for ($i=0; $i < count($aboutIntro); $i++) { 
 						$var = $i>3 ? $i-3 : $i;
 						if ($var == 0) {
 							echo "<div class = 'about-intro-description align-info'>
@@ -73,14 +119,28 @@
 						} 
 						echo $aboutIntro[$i];
 						echo "</div> </div>";
-					}
+					}*/
 				?>
-				</div>
+				</div>-->
 			</div>
 			<!-- About Work Steps -->
 			<div class ="about-work">
 				<h1 class="page-title">Cómo trabajamos</h1>
-				<div class ="work-steps">					
+				<div class = "step-numbers">
+					<div class ="step-number-conf title-step-one">
+						<p>1</p>
+					</div>					
+					<div class ="step-number-conf title-step-two">
+						<p>2</p>
+					</div>	
+					<div class ="step-number-conf title-step-three">
+						<p>3</p>
+					</div>	
+					<div class ="step-number-conf title-step-four">
+						<p>4</p>
+					</div>	
+				</div>
+				<div class ="work-steps">	
 					<div class ="step circle-one">
 						<h2 class ="circle-one work-step-title  title-step-one">Contactanos</h2>
 						<p class ="work-step-text">Si estas interesado en ayudarte a reacomodar tu habitación podes contactarte con nosotros.</p>
