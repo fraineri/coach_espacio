4cbf4e9b04dd336fb859e8cb7c40fc7a1fc63ecd
dianacrego <dianacrego@gmail.com>
2017-06-06 17:19:41 -0300

    construí varios de los métodos de dbSql y UserSql

---
+++ php/ignorar/UserSql.php
@@ -0,0 +1,10 @@
+<?php
+/*no funciona, sólo planteo*/
+require_once 'dbSQL.php';
+
+class SQLUser extends dbSQL {
+	public $tableName;
+
+/* 4- ¿qué hace el método UserSql(tableName, pdo)???*/
+	public function 
+}
--- php/ignorar/dbSQL.php
+++ php/ignorar/dbSQL.php
@@ -1,16 +1,26 @@
 <?php
+require_once 'database.php';
 
+/* 1- ¿es abstract o no??? porq en gráfico es abstract*/
 class dbSQL extends Database{
 	public $pdo;
 
 	function __construct ($table){
 		$this->$tableName = $table;
-		//$pdo = new conecction;
-		$this->setPrimaryKey();
+		//2- ¿me falta un this??
+		try { $pdo = new PDO('mysql:host=localhost;dbname=db_coach_espacio;charset=utf8mb4','root','123456');
+			} catch (PDOException $e) {
+			echo "<h1>La base de datos no está disponible</h1>";
+			}
+		$this->setPrimaryKey();/* setPrimaryKey la hereda de database*/
 	}
 
 	public function save($model){
-		//INSERT 
+		/*necesito un parámetro que sea tipo array para AGREGARLO o Modificar la tabla, 
+		pero no TODO el array de array. Si regrabo toda la tabla creo q tardaría mucho*/
+		$queIns= $pdo->query('INSERT INTO $tableName (id_usuarios, nombre, apellido, 
+			usuario, email, password, cambiar_pass, path, picture, mailing) 
+			VALUES (default,/* 3- ¿faltan los valores???*/)');
 	}
 
 	public function update($model){
@@ -18,14 +28,28 @@ class dbSQL extends Database{
 	}
 
 	public function find($value){
-		//SELECT * FROM $tableName WHERE primaryKey = $value;
+		$query = $pdo->query('SELECT * FROM $tableName WHERE primaryKey =$value');
+		/*devuelve objeto, con Fetch_Assoc lo paso a un 
+		Array cuyos índices son los nombres de las columnas de la Tabla*/
+		$results = $query->fetch(PDO::FETCH_ASSOC);
+		/* devuelve un array*/
+		return $results;
 	}
 
 	public function findAll(){
-		//SELECT * FROM $tableName
+		$query = $pdo->query('SELECT * FROM $tableName');
+		/*devuelve objeto, con Fetch_Assoc lo paso a un 
+		Array de Arrays cuyos índices son los nombres de las columnas de la Tabla*/
+		$results = $query->fetchAll(PDO::FETCH_ASSOC);
+		return $results;
 	}
 
 	public function findClause($campo, $value){
-		//SELECT * FROM $tableName WHERE $campo = $value
+		$query = $pdo->query('SELECT * FROM $tableName WHERE $campo = $value');
+		/*devuelve objeto, con Fetch_Assoc lo paso a un 
+		Array cuyos índices son los nombres de las columnas de la Tabla*/
+		$results = $query->fetch(PDO::FETCH_ASSOC);
+		/* devuelve array*/
+		return $results;
 	}
 }
